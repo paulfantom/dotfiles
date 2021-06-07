115accae74a5a45a514d43e806437dccdf94c7bd
paulfantom <pawel@krupa.net.pl>
2021-06-07 12:48:36 +0200

    shellcheck exclusion

--- install.sh
+++ install.sh
@@ -81,9 +81,11 @@ base() {
     ANS="N"
     read -t 30 -r -p "Do you want to remove KDE bloatware? [Y]es/[N]o  " -n 1 ANS || :
     if [[ "$ANS" =~ Y|y ]]; then
+    	#shellcheck disable=SC2046
         dnf remove -y $(cat "packages/remove.txt")
     fi
 
+    #shellcheck disable=SC2046
     dnf install -y $(cat "packages/install.txt")
 
     systemctl enable --now haveged
